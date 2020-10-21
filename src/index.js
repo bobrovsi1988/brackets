c2781a06c5780c4d833126a7a63c73751a2c0bfb
bobrovsi1988 <bobrovsi1988@gmail.com>
2020-10-22 01:15:05 +0300

    init:ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let separ=[];
+  bracketsConfig.forEach(element => {
+    separ.push(element.join(""));
+  });
+  //console.log(array);
+    
+    let result =str;
+  let CutBracketStr =(string, reg)=>{
+    let index =string.indexOf(reg, 0);
+    console.log(index);
+    if(index>0){
+      string = string.slice(0, index) + string.slice(index+2);
+    }
+    if(index===0){
+      string = string.slice(0, index) + string.slice(index+2);
+    }
+    return string;
+  }
+  for(let i=0; i<str.length; i++){
+    //console.log("res" + result);
+    
+    separ.forEach((el)=>{result =CutBracketStr(result, el)});
+  }
+   
+    // //replaceBracketsToString(bracketsConfig);
+   return !(result.length>0);
+  }
